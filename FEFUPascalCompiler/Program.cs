f82601f6952373573c64db70876114722e77fe0a
truefinch <glushkov.vk@students.dvfu.ru>
2018-11-07 02:35:05 +1000

    Add simple ui to use compiler

--- FEFUPascalCompiler/Program.cs
+++ FEFUPascalCompiler/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.IO;
+using System.Runtime;
+using FEFUPascalCompiler.Lexer;
+using FEFUPascalCompiler.Tokens;
 
 namespace FEFUPascalCompiler
 {
-    class Program
+    internal class FEFUPascalCompiler
     {
-        static void Main(string[] args)
+        private LexerDFA _lexer;
+
+        public FEFUPascalCompiler(StreamReader input)
         {
-            Console.WriteLine("Hello World!");
+            _lexer = new LexerDFA(input);
+        }
+
+        public void Next()
+        {
+            _lexer.NextToken();
+        }
+
+        public Token Peek()
+        {
+            return _lexer.PeekToken();
+        }
+    }
+    internal static class Application
+    {
+        private static void Help()
+        {
+            Console.WriteLine("-=|HELP|=-");
+            Console.WriteLine("dotnet FEFUPascalCompiler.dll [OPTIONS] project.pas");
+            Console.WriteLine("-=|options|=-");
+            Console.WriteLine("-l             run only lexer");
+            Console.WriteLine("-o filename    create output file");
+        }
+        
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Help();
+                return 0;
+            }
+
+            var inputFilePath = args[args.Length - 1];
+            try
+            {
+                var inputStream = File.OpenText(inputFilePath);
+                FEFUPascalCompiler compiler = new FEFUPascalCompiler(inputStream);
+
+                Token token = null;
+                while (!(token is EOFToken))
+                {
+                    try
+                    {
+                        token = compiler.Peek();
+                        compiler.Next();
+                        Console.WriteLine("{0},{1}\t{2}\t{3}\t{4}", 
+                            token.Line.ToString(), 
+                            token.Column.ToString(),
+                            token.TokenType.ToString(),
+                            token.StrValue,
+                            token.Text);
+                    }
+                    catch (LexerException exception) 
+                    {
+                        Console.WriteLine(exception.Message());
+                    }
+                }
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine($"{exception.FileName} not found");
+            }
+
+            return 0;
         }
     }
 }
