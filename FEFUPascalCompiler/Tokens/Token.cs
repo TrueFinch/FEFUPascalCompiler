03220b5fc603cbf42b99eb608954827b17fab467
truefinch <glushkov.vk@students.dvfu.ru>
2018-12-11 07:16:20 +1000

    Rewrite Token fabric from static function in static Dictionary Add ToString method to all Tokens Add new tokens

--- FEFUPascalCompiler/Tokens/Token.cs
+++ FEFUPascalCompiler/Tokens/Token.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq.Expressions;
 using FEFUPascalCompiler.Lexer;
 
 namespace FEFUPascalCompiler.Tokens
 {
-    //TODO: refactor structure of Token class and it's children to make them usable in unit testing
     public abstract class Token
     {
         public int Line { get; }
@@ -15,110 +12,68 @@ namespace FEFUPascalCompiler.Tokens
 
         public TokenType TokenType { get; protected set; }
 
-        public string Text { get; }
+        public string Lexeme { get; }
 
-        public string StrValue { get; protected set; }
-
-        protected Token(int line, int column, string text)
+        protected Token(int line, int column, string lexeme)
         {
             Line = line;
             Column = column;
-            Text = text;
+            Lexeme = lexeme;
         }
 
-        protected Token(int line, int column, TokenType tokenType, string text)
+        protected Token(int line, int column, TokenType tokenType, string lexeme)
         {
             Line = line;
             Column = column;
             TokenType = tokenType;
-            Text = text;
+            Lexeme = lexeme;
         }
 
-        public static Token GetToken(int line, int column, TokenType tokenType, string text)
+        public override string ToString()
         {
-            if (tokenType == TokenType.TYPE_INTEGER)
-            {
-                return new IntegerToken(line, column, text);
-            }
-
-            if (tokenType == TokenType.TYPE_DOUBLE)
-            {
-                return new DoubleToken(line, column, text);
-            }
-
-            if (tokenType == TokenType.TYPE_STRING)
-            {
-                return new StringConstToken(line, column, text);
-            }
-
-            if ((tokenType == TokenType.BIN_ARITHM_OPERATOR)
-                || (tokenType == TokenType.BIN_ARITHM_SUM)
-                || (tokenType == TokenType.BIN_ARITHM_DIF)
-                || (tokenType == TokenType.BIN_ARITHM_MUL)
-                || (tokenType == TokenType.BIN_ARITHM_DIV)
-                || (tokenType == TokenType.BIN_ARITHM_POW))
-            {
-                return new BinArithmeticOperationToken(line, column, text);
-            }
-
-            if ((tokenType == TokenType.ASSIGNMENT)
-                || (tokenType == TokenType.SMP_ASSIGN)
-                || (tokenType == TokenType.SUM_ASSIGN)
-                || (tokenType == TokenType.DIF_ASSIGN)
-                || (tokenType == TokenType.MUL_ASSIGN)
-                || (tokenType == TokenType.DIV_ASSIGN))
-            {
-                return new AssignToken(line, column, text);
-            }
-
-            if ((tokenType == TokenType.IDENT))
-            {
-                return new IdentToken(line, column, text);
-            }
-
-            if ((tokenType == TokenType.SEPARATOR)
-                || (tokenType == TokenType.DOT)
-                || (tokenType == TokenType.SEMICOLON)
-                || (tokenType == TokenType.COLON)
-                || (tokenType == TokenType.COMMA))
-            {
-                return new SeparatorToken(line, column, text);
-            }
-
-            throw new ArgumentOutOfRangeException(nameof(tokenType), tokenType, null);
+            return $"{(Line, Column), -10}| {TokenType, -20}| {Lexeme, -30}| ";
         }
+
+        public static readonly Dictionary<TokenType, Func<int, int, string, Token>> TokenConstructors =
+            new Dictionary<TokenType, Func<int, int, string, Token>>
+            {
+                {TokenType.IntegerNumber, (line, column, lexeme) => new IntegerNumberToken(line, column, lexeme)},
+                {TokenType.DoubleNumber, (line, column, lexeme) => new DoubleNumberToken(line, column, lexeme)},
+                {TokenType.StringConst, (line, column, lexeme) => new StringConstToken(line, column, lexeme)},
+                {TokenType.BinOperator, (line, column, lexeme) => new BinOperatorToken(line, column, lexeme)},
+                {TokenType.AssignOperator, (line, column, lexeme) => new AssignToken(line, column, lexeme)},
+                {TokenType.Separator, (line, column, lexeme) => new SeparatorToken(line, column, lexeme)},
+                {TokenType.Ident, (line, column, lexeme) => new IdentToken(line, column, lexeme)},
+                {TokenType.MultiLineComment, (line, column, lexeme) => new MultilineCommentToken(line, column, lexeme)},
+                {TokenType.SingleLineComment, (line, column, lexeme) => new SingleLineCommentToken(line, column, lexeme)},
+                {TokenType.Bracket, (line, column, lexeme) => new BracketToken(line, column, lexeme)}
+            };
     }
 
-    public class IntegerToken : Token
+    public class IntegerNumberToken : Token
     {
-        //base if the number system
-        private static Dictionary<char, int> basis = new Dictionary<char, int>
+        public IntegerNumberToken(int line, int column, string lexeme)
+            : base(line, column, TokenType.IntegerNumber, lexeme)
         {
-            {'%', 2}, {'&', 8}, {'$', 16},
-            {'0', 10}, {'1', 10}, {'2', 10}, {'3', 10}, {'4', 10}, {'5', 10}, {'6', 10}, {'7', 10}, {'8', 10}, {'9', 10}
-        };
+            Value = ParseAndCheckLexeme(lexeme);
+        }
 
-        private string ParseAndCheckLexeme(string lexeme)
+        private long ParseAndCheckLexeme(string lexeme)
         {
-            string str;
             try
             {
                 if (lexeme[0] == '-')
                 {
-                    str = (-1 * Convert.ToInt64(lexeme.Substring(2), basis[lexeme[1]])).ToString();
+                    return -1 * Convert.ToInt64(lexeme.Substring(2), basis[lexeme[1]]);
                 }
-                else
+
+                //if number not in decimal basis then lexeme[0] == % or $ or &
+                if (basis[lexeme[0]] != 10)
                 {
-                    //if number not in decimal basis then lexeme[0] == % or $ or &
-                    if (basis[lexeme[0]] != 10)
-                    {
-                        str = Convert.ToInt64(lexeme.Substring(1), basis[lexeme[0]]).ToString();
-                    }
-                    else
-                    {
-                        str = Convert.ToInt64(lexeme, basis[lexeme[0]]).ToString();
-                    }
+                    return Convert.ToInt64(lexeme.Substring(1), basis[lexeme[0]]);
                 }
+
+                return Convert.ToInt64(lexeme, basis[lexeme[0]]);
             }
             catch (FormatException exception)
             {
@@ -128,25 +83,37 @@ namespace FEFUPascalCompiler.Tokens
             {
                 throw new StrToIntConvertException($"Error on ({Line},{Column}) in {lexeme}: {exception.Message}");
             }
-
-            return str;
         }
 
-        public IntegerToken(int line, int column, string text)
-            : base(line, column, text)
+        public override string ToString()
         {
-            TokenType = TokenType.TYPE_INTEGER;
-            StrValue = ParseAndCheckLexeme(text);
+            return base.ToString() + $"{Value, -30}" + '|';
         }
+
+        public long Value { get; }
+
+        //base of the number system
+        private static Dictionary<char, int> basis = new Dictionary<char, int>
+        {
+            {'%', 2}, {'&', 8}, {'$', 16},
+            {'0', 10}, {'1', 10}, {'2', 10}, {'3', 10}, {'4', 10}, {'5', 10}, {'6', 10}, {'7', 10}, {'8', 10},
+            {'9', 10}
+        };
     }
 
-    public class DoubleToken : Token
+    public class DoubleNumberToken : Token
     {
-        private void CheckLexeme(string lexeme)
+        public DoubleNumberToken(int line, int column, string lexeme)
+            : base(line, column, TokenType.DoubleNumber, lexeme)
+        {
+            Value = LexemeToDouble(lexeme);
+        }
+
+        private double LexemeToDouble(string lexeme)
         {
             try
             {
-                Convert.ToDouble(lexeme);
+                return Convert.ToDouble(lexeme);
             }
             catch (FormatException exception)
             {
@@ -158,69 +125,141 @@ namespace FEFUPascalCompiler.Tokens
             }
         }
 
-        public DoubleToken(int line, int column, string text)
-            : base(line, column, text)
+        public override string ToString()
         {
-            TokenType = TokenType.TYPE_DOUBLE;
-            CheckLexeme(text);
-            StrValue = text;
+            return base.ToString() + $"{Value, -30}" + '|';
         }
+
+        public double Value { get; }
     }
 
     public class IdentToken : Token
     {
-        public IdentToken(int line, int column, string text)
-            : base(line, column, text)
+        public IdentToken(int line, int column, string lexeme)
+            : base(line, column, Dictionaries.KeyWords.ContainsKey(lexeme.ToLower())
+                    ? Dictionaries.KeyWords[lexeme.ToLower()]
+                    : TokenType.Ident,
+                lexeme)
         {
-            if (Dictionaries.KeyWords.ContainsKey(text.ToLower()))
-            {
-                TokenType = Dictionaries.KeyWords[text.ToLower()];
-            }
-            else
-            {
-                TokenType = TokenType.IDENT;
-            }
-
-            StrValue = text.ToLower();
+            Value = lexeme.ToLower();
         }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
     }
 
-    public class BinArithmeticOperationToken : Token
+    public class BinOperatorToken : Token
     {
-        public BinArithmeticOperationToken(int line, int column, string text)
-            : base(line, column, text)
+        public BinOperatorToken(int line, int column, string lexeme)
+            : base(line, column, Dictionaries.LexemeToTokenType[lexeme.ToLower()], lexeme)
         {
-            TokenType = Dictionaries.BinArithmeticOperators[text];
-            StrValue = text;
+            Value = lexeme.ToLower();
         }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
     }
 
     public class AssignToken : Token
     {
-        public AssignToken(int line, int column, string text)
-            : base(line, column, text)
+        public AssignToken(int line, int column, string lexeme)
+            : base(line, column, Dictionaries.LexemeToTokenType[lexeme.ToLower()], lexeme)
         {
-            TokenType = Dictionaries.Assigns[text];
-            StrValue = text;
+            Value = lexeme.ToLower();
         }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
     }
 
     public class SeparatorToken : Token
     {
-        public SeparatorToken(int line, int column, string text) : base(line, column, text)
+        public SeparatorToken(int line, int column, string lexeme)
+            : base(line, column, Dictionaries.LexemeToTokenType[lexeme.ToLower()], lexeme)
         {
-            StrValue = text;
-            TokenType = Dictionaries.Separators[text];
+            Value = lexeme.ToLower();
         }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
     }
 
     public class StringConstToken : Token
     {
-        public StringConstToken(int line, int column, string text)
-            : base(line, column, text)
+        public StringConstToken(int line, int column, string lexeme)
+            : base(line, column, TokenType.StringConst, lexeme)
         {
-            TokenType = TokenType.TYPE_STRING;
-            StrValue = text;
+            Value = lexeme;
         }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
+    }
+
+    public class MultilineCommentToken : Token
+    {
+        public MultilineCommentToken(int line, int column, string lexeme)
+            : base(line, column, TokenType.MultiLineComment, lexeme)
+        {
+            Value = lexeme;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
+    }
+    
+    public class SingleLineCommentToken : Token
+    {
+        public SingleLineCommentToken(int line, int column, string lexeme)
+            : base(line, column, TokenType.SingleLineComment, lexeme)
+        {
+            Value = lexeme;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
+    }
+    
+    public class BracketToken: Token{
+        public BracketToken(int line, int column, string lexeme)
+            : base(line, column, Dictionaries.LexemeToTokenType[lexeme.ToLower()], lexeme)
+        {
+            Value = lexeme.ToLower();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Value, -30}" + '|';
+        }
+
+        public string Value { get; }
     }
 }
