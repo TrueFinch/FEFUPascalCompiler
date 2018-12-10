0b98eab5677d149faed4adcc242db97963da2571
truefinch <glushkov.vk@students.dvfu.ru>
2018-12-11 07:18:49 +1000

    Move compiler class to new file Rewrite try ... catch operators - now program will not fall with exceptions i run-time but will be printed error message and program will be stopped

---
+++ FEFUPascalCompiler/FEFUPascalCompiler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using FEFUPascalCompiler.Lexer;
+using FEFUPascalCompiler.Tokens;
+
+namespace FEFUPascalCompiler
+{
+    public class FEFUPascalCompiler
+    {
+        private LexerDfa _lexer;
+
+        public FEFUPascalCompiler()
+        {
+            _lexer = new LexerDfa();
+        }
+
+        public void SetInput(string inputFilePath)
+        {
+            try
+            {
+                var inputStream = File.OpenText(inputFilePath);
+                _lexer.SetInput(ref inputStream);
+//                Next();
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        public bool Next()
+        {
+            return _lexer.NextToken();
+        }
+
+        public Token Peek()
+        {
+            return _lexer.PeekToken();
+        }
+    }
+}
