e1aaff6149324e17efbb1331b04cc9dde06d252b
GlushkovVK <glushkov.vk@students.dvfu.ru>
2019-01-08 15:24:52 +1000

    Little refactor

--- FEFUPascalCompiler/Tokens/Token.cs
+++ FEFUPascalCompiler/Tokens/Token.cs
@@ -11,7 +11,7 @@ namespace FEFUPascalCompiler.Tokens
 
         public int Column { get; }
 
-        public TokenType TokenType { get; protected set; }
+        public TokenType Type { get; protected set; }
 
         public string Lexeme { get; }
 
@@ -22,17 +22,17 @@ namespace FEFUPascalCompiler.Tokens
             Lexeme = lexeme;
         }
 
-        protected Token(int line, int column, TokenType tokenType, string lexeme)
+        protected Token(int line, int column, TokenType type, string lexeme)
         {
             Line = line;
             Column = column;
-            TokenType = tokenType;
+            Type = type;
             Lexeme = lexeme;
         }
 
         public override string ToString()
         {
-            return $"{(Line, Column),-10}| {TokenType,-20}| {Lexeme,-30}| ";
+            return $"{(Line, Column),-10}| {Type,-20}| {Lexeme,-30}| ";
         }
 
         public static readonly Dictionary<TokenType, Func<int, int, string, Token>> TokenConstructors =
