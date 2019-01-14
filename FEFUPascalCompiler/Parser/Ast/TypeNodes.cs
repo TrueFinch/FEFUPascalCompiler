c4d8ce17141025586651553e6cbca043f513b0d2
GlushkovVK <glushkov.vk@students.dvfu.ru>
2019-01-14 22:13:08 +1000

    Add simpleType FuncSignature and ProcSignature nodes

--- FEFUPascalCompiler/Parser/Ast/TypeNodes.cs
+++ FEFUPascalCompiler/Parser/Ast/TypeNodes.cs
@@ -5,12 +5,20 @@ using Microsoft.VisualBasic.CompilerServices;
 
 namespace FEFUPascalCompiler.Parser
 {
-//    public abstract class TypeNode : AstNode
-//    {
-//        protected TypeNode(AstNodeType type) : base(type)
-//        {
-//        }
-//    }
+    public class SimpleType : AstNode
+    {
+        public SimpleType(AstNode typeIdent) : base(AstNodeType.SimpleType, token)
+        {
+            _children.Add(typeIdent);
+        }
+
+        public override T Accept<T>(IAstVisitor<T> visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        public AstNode TypeIdent => _children[0];
+    }
 
     public class ArrayType : AstNode
     {
@@ -71,7 +79,7 @@ namespace FEFUPascalCompiler.Parser
         public FieldSection(Token colon, AstNode identList, AstNode identsType) : base(AstNodeType.FieldSection)
         {
             Colon = colon;
-            
+
             _children.Add(identList);
             _children.Add(identsType);
             Value = colon.Value;
@@ -81,7 +89,7 @@ namespace FEFUPascalCompiler.Parser
         {
             return visitor.Visit(this);
         }
-        
+
         public Token Colon { get; }
         public AstNode Idents => _children[0];
         public AstNode IdentsType => _children[1];
@@ -100,8 +108,41 @@ namespace FEFUPascalCompiler.Parser
         {
             return visitor.Visit(this);
         }
-        
+
         public Token Carriage { get; }
         public AstNode SimpleType => _children[0];
+    }
+
+    public class ProcSignature : AstNode
+    {
+        public ProcSignature(Token token, List<AstNode> paramSections) : base(AstNodeType.ProcSignature, token)
+        {
+            _children.InsertRange(0, paramSections);
+        }
+
+        public override T Accept<T>(IAstVisitor<T> visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        public List<AstNode> ParamList => _children;
+    }
+
+    public class FuncSignature : AstNode
+    {
+        public FuncSignature(Token token, List<AstNode> paramSections, AstNode returnType)
+            : base(AstNodeType.ProcSignature, token)
+        {
+            _children.InsertRange(0, paramSections);
+            _children.Add(returnType);
+        }
+
+        public override T Accept<T>(IAstVisitor<T> visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        public List<AstNode> ParamList => _children.GetRange(0, _children.Count - 1);
+        public AstNode ReturnType => _children[_children.Count - 1];
     }
 }
